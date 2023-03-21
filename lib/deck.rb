e7e50945167d20d24b975c735a6691438a064f0c
William Zale <williamzale@gmail.com>
2023-03-21 18:06:20 -0400

    Add outlines of methods to create

---
+++ lib/deck.rb
@@ -0,0 +1,28 @@
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+def rank_of_card_at(number)
+  cards[number].rank
+end
+
+def high_ranking_cards
+  if card.rank > 10
+    card
+  end
+end
+
+def percent_high_ranking
+
+end
+
+def remove_card
+
+end
+
+def add_card
+
+end
+end
